7c344d1e8afa94d20ca5f648aa783d3ee036052f
DerekOung <derekoung2016@u.northwestern.edu>
2017-03-03 11:32:44 -0600

    Surface fit function with comments

--- fit_surface.m
+++ fit_surface.m
@@ -1,5 +1,10 @@
 function [ fitobject, gof, output ] = fit_surface( x, y, z, char )
-% Fits a surface given z values of points in x, y
+% Fits a surface
+% Inputs
+% char (fit type, ex: 'poly33' for polynomial of order 3 for x and y)
+% x x location
+% y y location
+% z z location
 
 fo = fitoptions('Method', 'LinearLeastSquares', ...
     'Robust', 'on');
