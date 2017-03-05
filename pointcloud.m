c5ca50121bba8d5d19785c8d1f754272dd85d6e4
Jacob Bruce <jacobbruce2016@u.northwestern.edu>
2017-03-05 16:30:44 -0600

    Added boolean display argument
    Shows plots if true

--- pointcloud.m
+++ pointcloud.m
@@ -1,6 +1,7 @@
-function raster = pointcloud(path)
+function raster = pointcloud(path, display)
 % wrapper function, creates DTM raster from path to pointcloud data
 % input: a path to the .fuse pointcloud file
+% a boolean indicating whether or not to display the output
 % output: gridded raster data
 
 % open the file
@@ -14,14 +15,19 @@ sizexyz = [4 Inf];
 xyzPoints = fscanf(fileID,formatSpec, sizexyz).';
 xyzPoints = xyzPoints(:,1:3);
 ptCloud = pointCloud(xyzPoints);
-pcshow(ptCloud);
+if display
+    pcshow(ptCloud);
+end
 
 % get x,y,z coordinates in meters from 0,0,0
 
 curr_points = coords_from_lat_lon(xyzPoints, ptCloud);
 curr_cloud = pointCloud(curr_points);
 init_cloud = curr_cloud;
-pcshow(curr_cloud);
+if display
+    figure();
+    pcshow(curr_cloud);
+end
 
 % loop start conditions
 
@@ -38,19 +44,25 @@ while avg_error > threshold
 
     % calculate gridded minimums 
     min_pts = get_minimums(curr_cloud, box_size);
-    figure();
-    pcshow(pointCloud(min_pts));
+    if display
+        figure();
+        pcshow(pointCloud(min_pts));
+    end
 
     % fit surface to gridded minimums
     [ fitobject, gof, output ] = fit_surface( min_pts(:,1), min_pts(:,2), min_pts(:,3), 'poly44');
-    figure();
-    plot(fitobject);
-
+    if display
+        figure();
+        plot(fitobject);
+    end
+    
     % filter by surface
     [curr_points,avg_error] = filter_by_surf(curr_points, fitobject, up_tol, down_tol);
     curr_cloud = pointCloud(curr_points);
-    figure();
-    pcshow(curr_cloud);  
+    if display
+        figure();
+        pcshow(curr_cloud);  
+    end
     
     % resetting for new loop
     up_tol = up_tol / 2;
